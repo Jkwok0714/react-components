0cc83606267b30657c212becd2d2d67b7ac455d7
Justin Kwok <jkwok0714@gmail.com>
2017-09-08 11:12:57 -0700

    React component tutorial complete

--- app.js
+++ app.js
@@ -1,1 +1,61 @@
 // TODO
+var GroceryList = (props) => (
+  <ul>
+    {props.list.map(listItem =>
+      <GroceryListItem listItem={listItem} />
+    )}
+  </ul>
+);
+
+// Static prop-taking version; stateless component
+// var GroceryListItem = (props) => (
+//   <li>{props.item[0]}</li>
+// );
+
+class GroceryListItem extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      hover: false
+    }
+  }
+
+  onListItemHover () {
+    this.setState({
+      hover: !this.state.hover
+    });
+  }
+
+  /*
+  render() {
+
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    };
+
+    return (
+      <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.todo}</li>
+    );
+  }
+  */
+  render () {
+    var style = {
+      fontWeight: this.state.hover ? 'bold' : 'normal'
+    };
+
+    return (
+      <li style={style}
+        onMouseEnter={this.onListItemHover.bind(this)}
+        onMouseLeave={this.onListItemHover.bind(this)}>{this.props.listItem}</li>
+    );
+  }
+}
+
+var App = () => (
+  <div>
+  <h2>Grocery List</h2>
+  <GroceryList list={["Candy", "Gelbanana", "Cup Noodle"]} />
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
